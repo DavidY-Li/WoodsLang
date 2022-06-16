237635f4eb9489c1ce44d04ca4e3d420d849b13f
BuildTools <davidliepic@gmail.com>
2022-06-15 22:06:45 -0400

    Broken stuff added

--- src/com/david/woods/Expr.java
+++ src/com/david/woods/Expr.java
@@ -59,11 +59,8 @@ abstract class Expr {
   }
   interface Visitor<R> {
     R visitBinaryExpr(Binary expr);
-
     R visitGroupingExpr(Grouping expr);
-
     R visitLiteralExpr(Literal expr);
-
     R visitUnaryExpr(Unary expr);
   }
 
